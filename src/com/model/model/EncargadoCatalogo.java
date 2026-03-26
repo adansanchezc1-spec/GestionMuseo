465fa3833cf2ba37a7ff1a2cfb0ed1dfcc516171
Adan <asanchez00@unisalle.edu.co>
2026-03-26 17:25:24 -0500

    Clase encargado Catalogo Creada

--- src/com/model/model/EncargadoCatalogo.java
+++ src/com/model/model/EncargadoCatalogo.java
@@ -1,5 +1,45 @@
 package com.model.model;
 
-public class EncargadoCatalogo {
-    
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+
+public class EncargadoCatalogo extends  {
+      public EncargadoCatalogo(String nombre) {
+        super(nombre, "ENCARGADO_CATALOGO");
+    }
+
+    @Override
+    public boolean autenticar(String contrasena) {
+        return contrasena != null && contrasena.length() >= 6;
+    }
+
+    /**
+     * Inscribe una obra en el catálogo.
+     *
+     * @param obra     obra a inscribir
+     * @param catalogo catálogo destino
+     */
+    public void inscribirObra(Obra obra, Catalogo catalogo) {
+        catalogo.agregarObra(obra);
+    }
+
+    /**
+     * Asigna un nuevo estado a una obra.
+     *
+     * @param obra        obra a modificar
+     * @param nuevoEstado estado destino
+     */
+    public void asignarEstado(Obra obra, EstadoObra nuevoEstado) {
+        obra.asignarEstado(nuevoEstado);
+    }
+
+    @Override
+    public List<String> mostrarMenu() {
+        return Collections.unmodifiableList(Arrays.asList(
+            "1. Inscribir nueva obra",
+            "2. Asignar estado a obra",
+            "3. Listar obras del catálogo"
+        ));
+    }
 }
